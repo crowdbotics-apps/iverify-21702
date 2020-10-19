08b094a15af3fa83b63ecad68c259f4259f399f3
Crowdbotics <team@crowdbotics.com>
2020-10-19 07:12:21 +0000

    Update models

--- backend/iverify_21702/wsgi.py
+++ backend/iverify_21702/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iverify_21702.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iverify_21702.settings")
 
 application = get_wsgi_application()
